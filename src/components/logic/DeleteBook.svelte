0e27bbb3aff8a47db21a7f69f12bdc225d34cc78
Bryan Gordon <bryanbge92@gmail.com>
2025-11-04 21:09:42 -0500

    Create component for delete a book

---
+++ src/components/logic/DeleteBook.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+
+  export let idBook: string = ''
+
+  async function HandleDeleteBook () {
+    try{
+      const resp = await fetch(`http://localhost:3000/books/${idBook}`)
+
+      if (resp.ok) {
+        alert("User deleted succesfully")
+      }
+      /** Probar si funciona agregando un return o no hace falta (colocar el return aqui)*/
+    }
+    catch (e) {
+      console.log(e)
+      alert("Could not delete book")
+    }
+  }
+
+</script>
+
+<div>
+  <button on:click={HandleDeleteBook}>
+    Delete
+  </button>
+</div>
+
+<style>
+  div {
+    text-align: center;
+    margin: 30px auto;
+    width: 25dvw;
+  }
+
+  button:hover {
+    background-color: #dc143c7c;
+  }
+
+  button {
+    background-color: crimson;
+    color: whitesmoke;
+    border-radius: 8px;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    transition: all ease 0.3s;
+  }
+</style>
