cb0e094e96ca299fd72dfef711a08d12bab3b0ec
Bryan Gordon <bryanbge92@gmail.com>
2025-11-05 20:50:41 -0500

    Add delete funcionality

---
+++ src/components/logic/DeleteUser.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  export let idUser: string = ''
+
+  async function HandleDeleteUser () {
+    try {
+      const resp = await fetch(`http://localhost:3000/${idUser}`, {
+        method: "DELETE"
+      })
+
+      if (resp.ok) {
+        alert("User deleted succesfully")
+        window.location.href="/admin/users"
+      }
+    }
+    catch(e) {
+      console.log(e)
+      alert("Could not delete user")
+    }
+  }
+
+</script>
+
+<div>
+  <button on:click={HandleDeleteUser}>
+    Delete
+  </button>
+</div>
+
+<style>
+  div {
+    text-align: center;
+    margin: 30px auto;
+    width: 25dvw;
+  }
+
+  button:hover {
+    background-color: #dc143c7c;
+  }
+
+  button {
+    background-color: crimson;
+    color: whitesmoke;
+    border-radius: 8px;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    transition: all ease 0.3s;
+  }
+</style>
