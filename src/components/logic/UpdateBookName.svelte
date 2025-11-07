502fa8dbbfcfd96072c727f87337263d8706a39e
Bryan Gordon <bryanbge92@gmail.com>
2025-11-06 22:16:31 -0500

    Add book name update functionality
    Implemented a form to update a book's name using a PATCH request to the backend. Added input binding, improved form styling, and provided user feedback on success or failure.

--- src/components/logic/UpdateBookName.svelte
+++ src/components/logic/UpdateBookName.svelte
@@ -1,16 +1,41 @@
 <script lang="ts">
 
+  export let idBook: string = ''
+  export let nameBook: string = ''
+
+  let newName: string = '' 
+
+  function HandleUpdateName (e: Event) {
+    e.preventDefault()
+
+    try {
+      fetch(`http://localhost:3000/books/update-name/${idBook}`, {
+        method: "PATCH",
+        body: JSON.stringify({name: newName}),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+
+      alert('Name update successfully')
+    }
+    catch(e) {
+      console.error(e)
+      alert('Could not update name')
+    }
+  }
+  
 </script>
 
 <section>
   <h1>Update book name</h1>
 
   <article>
-    <h3>Current Name: </h3>
+    <h3>Current Name: {nameBook}</h3>
 
-    <form>
+    <form on:submit={HandleUpdateName}>
       <label for="updated-name">New name:</label>
-      <input type="text">
+      <input id="updated-name" type="text" bind:value={newName}/>
       <button type="submit">Update</button>
     </form>
   </article>
@@ -44,9 +69,22 @@
 
   input {
     width: 55%;
+    border-radius: 8px;
+    padding: 6px;
+    border: none;
+  }
+
+  button:hover {
+    background-color: #90ee90bc;
+
   }
 
   button {
     width: 20%;
+    border-radius: 8px;
+    padding: 10px;
+    background-color: lightgreen;
+    cursor: pointer;
+    transition: all ease 0.3s;
   }
 </style>
