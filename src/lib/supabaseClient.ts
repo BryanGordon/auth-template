caf90fd1b47f46701d21e6f3231ef28b06c3c6d3
Bryan Gordon <bryanbge92@gmail.com>
2025-11-13 22:43:11 -0500

    Use public env vars for Supabase client config
    Updated supabaseClient.ts to use PUBLIC_SUPA_URL and PUBLIC_SUPA_KEY from environment variables for Supabase initialization.

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseURL = import.meta.env
-const supaKey = import.meta.env
+const supabaseURL = import.meta.env.PUBLIC_SUPA_URL
+const supaKey = import.meta.env.PUBLIC_SUPA_KEY
 
 export const supabase = createClient(supabaseURL, supaKey)
