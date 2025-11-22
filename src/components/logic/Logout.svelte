e70cba271acbd1cda2d285e3cea81a1c9ca3161e
Bryan Gordon <bryanbge92@gmail.com>
2025-11-21 22:13:15 -0500

    Create component for logout management

---
+++ src/components/logic/Logout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+  import { supabase } from '../../lib/supabaseClient'
+  
+  async function logout() {
+    await supabase.auth.signOut()
+    window.location.href = "/"
+  }
+
+  onMount(() => {
+    logout()
+  })
+
+</script>
+
+<h3>
+  Logging out...
+</h3>
