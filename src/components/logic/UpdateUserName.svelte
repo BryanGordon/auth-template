6a9131afe3e0ab4737f8c2c4462dda0d359cf2a7
Bryan Gordon <bryanbge92@gmail.com>
2025-12-01 22:29:43 -0500

    Add auth

--- src/components/logic/UpdateUserName.svelte
+++ src/components/logic/UpdateUserName.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-  export let nameUser: string = ''
+    import { onMount } from 'svelte';
+    import { supabase } from '../../lib/supabaseClient';
+
   export let userId: string = ''
 
   let newUserName: string = ''
+  let token: string | undefined = undefined
+  let load = true
+  let isAdmin = false
+  let user = {name: ''}
 
   function HandleUpdateUserName (e: Event) {
     e.preventDefault()
@@ -23,13 +29,74 @@
       alert("Could not update name")
     }
   }
+
+  onMount(async () => {
+    const { data } = await supabase.auth.getSession()
+    token = data.session?.access_token
+
+    if (!token) {
+      window.location.href = "/"
+      return
+    }
+
+    try {
+      const roleResponse = await fetch('http://localhost:3000/login', {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      const { rol } = await roleResponse.json()
+
+      if (rol != "admin") {
+        window.location.href = "/user"
+        return
+      }
+
+    }
+
+    catch(e) {
+      console.error(e)
+      alert("Error getting credentials")
+      window.location.href = "/admin/users"
+      return
+    }
+
+    isAdmin = true
+
+    try {
+      const response = await fetch(`http://localhost:3000/admin/users/search/${userId}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      const data = await response.json()
+      user = data[0]
+    }
+
+    catch(e) {
+      console.error(e)
+      alert("Error fetching user data")
+      window.location.href = "/admin/users"
+      return
+    }
+
+    load = false
+
+  })
 </script>
 
 <section>
+  {#if load}
+    <p>Loading...</p>
+  {:else if isAdmin}
   <h1>Update book name</h1>
 
   <article>
-    <h3>Current Name: {nameUser}</h3>
+    <h3>Current Name: {user.name}</h3>
 
     <form on:submit={HandleUpdateUserName}>
       <label for="updated-name">New name:</label>
@@ -37,6 +104,9 @@
       <button type="submit">Update</button>
     </form>
   </article>
+  {:else}
+    <p>Unauthorized</p>
+  {/if}
 </section>
 
 <style>
