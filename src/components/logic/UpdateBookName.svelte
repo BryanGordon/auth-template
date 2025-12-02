5db7c337ee7fdba7246fee75818039e2d637882d
Bryan Gordon <bryanbge92@gmail.com>
2025-12-01 22:29:31 -0500

    Add auth

--- src/components/logic/UpdateBookName.svelte
+++ src/components/logic/UpdateBookName.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import { supabase } from '../../lib/supabaseClient';
 
 
   export let idBook: string = ''
-  export let nameBook: string = ''
 
   let newName: string = ''
+  let token: string | undefined = undefined
+  let nameBook = {name: ''}
+  let load = true
+  let isAdmin = false
 
   function HandleUpdateName (e: Event) {
     e.preventDefault()
@@ -15,7 +19,8 @@
         method: "PATCH",
         body: JSON.stringify({name: newName}),
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
         }
       })
 
@@ -27,14 +32,68 @@
     }
   }
   
-  onMount(async () => {})
+  onMount(async () => {
+    const { data } = await supabase.auth.getSession()
+    token = data.session?.access_token
+
+    if (!token) {
+      window.location.href = "/"
+      return
+    }
+
+    try {
+      const rolResponse = await fetch('http://localhost:3000/login', {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      const { rol } = await rolResponse.json()
+
+      if (rol != "admin") {
+        window.location.href = "/user"
+        return
+      }
+    }
+
+    catch(e) {
+      console.error(e)
+      alert("Error getting credentials")
+    }
+
+    isAdmin = true
+
+    try {
+      const response = await fetch(`http://localhost:3000/search/books/${idBook}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      const book = await response.json()
+      nameBook = book[0]
+    }
+
+    catch(e) {
+      console.error(e)
+      alert("Error getting book information")
+    }
+
+    load = false
+
+  })
 </script>
 
 <section>
+  {#if load}
+    <p>Loading...</p>
+  {:else if isAdmin}
   <h1>Update book name</h1>
 
   <article>
-    <h3>Current Name: {nameBook}</h3>
+    <h3>Current Name: {nameBook.name}</h3>
 
     <form on:submit={HandleUpdateName}>
       <label for="updated-name">New name:</label>
@@ -42,6 +101,9 @@
       <button type="submit">Update</button>
     </form>
   </article>
+  {:else}
+    <p>Unauthorized</p>
+  {/if}
 </section>
 
 <style>
